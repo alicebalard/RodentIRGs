a0855b6c3f67415403381f239c43af3d0249d51d
Alice <alice.balard@fu-berlin.de>
2021-02-02 16:48:10 +0100

    add script folder

---
+++ code/1_tblastnIRGs.sh
@@ -0,0 +1,8 @@
+# Identify sequences from Bekpen to the newest Mus musculus genome
+
+
+# Retrieve sequences from the well-annotated Microtus ochrogaster genome
+# Get genomes for our 9 rodent species (if several genomes are available, take them?)
+# tblastn mus IRG in that
+# tblastn microtus IRG in that
+
