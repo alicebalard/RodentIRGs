ea0cb3a1539c7038e4b5dcae5807606575e6c072
Alice <alice.balard@fu-berlin.de>
2021-02-09 08:57:00 +0100

    add Mmus_GRCm39_IRG_prot_names_corresp_orthofinder.csv

--- code/1_tblastnIRGs.sh
+++ code/1_tblastnIRGs.sh
@@ -21,3 +21,12 @@
 ## concatenate
 # for i in *.fasta; do cat $i >> concatenatedtblastn.fasta; done
 
+## C. Run orthofinder
+### The files from Ensembl will contain many transcripts per gene. If we ran OrthoFinder on these raw files it would take 10x longer than necessary and could lower the accuracy. We’ll use a script provided with OrthoFinder to extract just the longest transcript variant per gene and run OrthoFinder on these files:
+# for f in *faa ; do python /localstorage/alice/anaconda3/pkgs/orthofinder-2.5.1-0/bin/primary_transcript.py $f ; done
+### Run Orthofinder
+# orthofinder -f primary_transcripts/
+
+## extract orthogroups
+# grep -E "NP_001028939.1|NP_001030031.2|NP_001094945.1|NP_068564.4|NP_001342686.1|NP_062313.3|NP_061208.3|NP_001039005.1|NP_001019401.2|NP_001108151.1|NP_035709.3|NP_775610.1|NP_001138636.1|NP_001258606.1|NP_001128587.1|NP_001344963.1|NP_694774.3" primary_transcripts/OrthoFinder/Results_Feb06/Orthogroups/Orthogroups.tsv > Mmus_GRCm39_IRG_prot_names_corresp_orthofinder.csv
+## -> gitted file
