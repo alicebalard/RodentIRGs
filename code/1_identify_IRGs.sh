605156fdae56d7e1304b093de8fb98183c58e3ee
Alice <alice.balard@fu-berlin.de>
2021-03-24 18:40:10 +0100

    add missing tblastn

--- code/1_identify_IRGs.sh
+++ code/1_identify_IRGs.sh
@@ -8,12 +8,9 @@
 # samtools faidx big_data/genomes/musMusculus/GCF_000001635.27_GRCm39_genomic.fna NC_000077.7:48909897-48911170 | seqtk seq -r
 
 ## B. Search for IRGs: TBLASTN search translated nucleotide databases using a protein query in genomes for our 9 rodent species 
-# for F in /SAN/Alices_sandpit/Torelli_transcriptomics/big_data/genomes/*.fna; do makeblastdb -in $F -dbtype nucl; done
-# for F in /SAN/Alices_sandpit/Torelli_transcriptomics/big_data/genomes/*.fna; do tblastn -query ../data/fasta/Protein_sequences_of_IRG_mouse_GRCm39.fasta -db $F -num_threads 16 -outfmt "6 qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore sseq" -out ../data/blast_results/tblstnIRGmusGRCm39_vs_${F%.*}.outfmt6
 
-## move results in good folder
-# for i in *outfmt6; do mv -- "$i" "${i/%.outfmt6/_queryIRG_GRCm39.outfmt6}"; done
-# moved in: RodentIRGs/data/blast_results/IRGmus2otherRodents
+# for F in /SAN/Alices_sandpit/Torelli_transcriptomics/big_data/genomes/*.fna; do makeblastdb -in $F -dbtype nucl; done
+# for F in /SAN/Alices_sandpit/Torelli_transcriptomics/big_data/genomes/*.fna; do tblastn -query /SAN/Alices_sandpit/Torelli_transcriptomics/GIT/RodentIRGs/data/fasta/Protein_sequences_of_IRG_mouse_GRCm39.fasta -db $F -num_threads 16 -outfmt "6 qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore sseq" -out /SAN/Alices_sandpit/Torelli_transcriptomics/GIT/RodentIRGs/data/blast_results/IRGmus2otherRodents/tblastnIRGmusGRCm39_vs_$(basename -- "$F").outfmt6; done
 
 ## transform in fasta
 # for myfile in *outfmt6; do awk 'BEGIN { OFS = "\n" } { print ">"substr(FILENAME,1,4)"_"$1" "$2":"$9"-"$10" evalue:"$11" mismatches:"$5, $13 }' $myfile > $myfile.fasta; done
