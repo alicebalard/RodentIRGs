422e6ec625c6a1e5959bca4d1222f4621b8180ba
Alice <alice.cam.balard@gmail.com>
2021-04-15 20:54:53 +0200

    correct bugs color synteny maps

--- code/keepBestBlast.R
+++ code/keepBestBlast.R
@@ -3,11 +3,7 @@
 
 library(dplyr)
 
-filenames <- list.files("../data/blast_results/IRGmus2otherRodents/row_tblasn", full.names=TRUE)
-
-substrRight <- function(x, n){
-  substr(x, nchar(x)-n+1, nchar(x))
-}
+filenames <- list.files("../data/blast_results/IRGmus2otherRodents/", full.names=TRUE)
 
 Results_blast_best <- data.frame()
 Results_blast_full <- data.frame()
@@ -15,15 +11,8 @@ Results_blast_full <- data.frame()
 for (f in filenames){
   blastdata <- read.delim(file = f, header = F)
   
-  # remove identical sequences search b5/b5*, b6/b6*, and   
-  # keep only "alternative" sequences for Irgb10 and Irgq (corresponds to NCBI)
-  blastdata <- blastdata[!blastdata$V1 %in% c("Irgb5*", "Irgb6*", "Irgb10", "Irgq"),]
-  blastdata$V1[blastdata$V1 %in% "Irgb10alternative(longer)"] <- "Irgb10"
-  blastdata$V1[blastdata$V1 %in% "Irgqalternative(longer)"] <- "Irgq"
-  
-  # remove "alternative" tandems (= concatenated protein) TO CHECK
-  blastdata <- blastdata[!grepl("alternative", blastdata$V1),]
-  
+  # remove identical sequences search b6/b6*, and   
+  blastdata <- blastdata[!blastdata$V1 %in%  "Irgb6*",]
   blastHeaders <- c("qseqid", "sseqid", "pident", "length", "mismatch", "gapopen", "qstart", "qend", "sstart", "send", "evalue", "bitscore", "sseq")
   colnames(blastdata) <- blastHeaders
   
@@ -44,8 +33,7 @@ for (f in filenames){
   whichCandidate <- data.frame(findOverlaps(fullblastRange, candidateDF, type="any"))
   blastdata$candidate <- whichCandidate$subjectHits
   
-  blastdata$Species <- substrRight(f, 4)
-  
+  blastdata$Species <- substr(sub(".*tBlastnIRGmusGRCm39_vs_", "", f), 1,4)
   
   # for each candidate IRG, keep the row with the highest bitscore (if by eval, keep the first randomly if eval = 0)
   blastdataFinal <- blastdata %>% group_by(candidate) %>% 
@@ -61,22 +49,16 @@ for (f in filenames){
 names(Results_blast_best)[names(Results_blast_best)%in% "sseqid"] <- "chr"
 ## (2)IrgGroup in "pseudogene", "IrgbTandem", "Irgb6-OG1", "Irgb6-OG2", "Irgb6-tbn","Irgb10",
 #  "Irga2-6", "Irga4", "Irga3", "Irgm1", "Irgc", "Irgm2", "Irgd", "Irgq", "Irgm3
-Results_blast_best$IrgGroup <- Results_blast_best$qseqid
-
-unique(Results_blast_best$qseqid)
-Results_blast_best$IrgGroup[Results_blast_best$IrgGroup %in% c("Irga1","Irga5","Irga8","Irga7", "Irgb7")] <- "pseudogene"
-Results_blast_best$IrgGroup[Results_blast_best$IrgGroup %in% c("Irga2","Irga6")] <- "Irga2-6"
-Results_blast_best$IrgGroup[
-  Results_blast_best$IrgGroup %in% 
-    c("Irgb9","Irgb8", "Irgb5*b3alternative", "Irgb8b9alternative", "Irgb1",
-      "Irgb5*","Irgb5", "Irgb4", "Irgb3", "Irgb2", "Irgb2b1alternative")] <- "IrgbTandem"
-Results_blast_best$IrgGroup[Results_blast_best$IrgGroup %in% "Irgb10alternative(longer)"] <- "Irgb10"
+Results_blast_best$IrgGroup <- gsub("_refMmus", "", gsub("_refMus", "", Results_blast_best$qseqid))
+Results_blast_best$IrgGroup[grepl("Irga1|Irga5|Irga8|Irga7|Irgb7", Results_blast_best$IrgGroup)] <- "pseudogene"
+Results_blast_best$IrgGroup[grepl("Irga2|Irga6", Results_blast_best$IrgGroup)] <- "Irga2-6"
+Results_blast_best$IrgGroup[Results_blast_best$IrgGroup %in% 
+                              c("Irgb2","Irgb1","Irgb4","Irgb5","Irgb3","Irgb9","Irgb8")] <- "IrgbTandem"
 Results_blast_best$IrgGroup[Results_blast_best$IrgGroup %in% "Irgb6*"] <- "Irgb6"
-Results_blast_best$IrgGroup[Results_blast_best$IrgGroup %in% "Irgqalternative(longer)"] <- "Irgq"
 
 ## (3) IrgName
 Results_blast_best <- data.frame(Results_blast_best %>% group_by(Species, qseqid) %>% mutate(id = row_number()))
-Results_blast_best$IrgName <- paste(Results_blast_best$qseqid, Results_blast_best$id, sep = ".")
+Results_blast_best$IrgName <- gsub("_refMus", "", paste(Results_blast_best$qseqid, Results_blast_best$id, sep = "."))
 
 ## (4) Species names
 Results_blast_best$Species[Results_blast_best$Species %in% "Aamp"] <- "A.amphibius"
@@ -94,4 +76,3 @@ Results_blast_best$Species[Results_blast_best$Species %in% "Rrat"] <- "R.rattus"
 
 ## WRITE OUT
 write.csv(Results_blast_best, "../data/data4syntheny_tblastn.csv", row.names = F)
-  
